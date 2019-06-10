4e34e454cf79e3cba217b003ade770155ce46d0e
jienfak <jienfak@protonmail.com>
2019-06-11 01:39:09 +0500

    modified:   .gitignore 	modified:   config.h

--- config.h
+++ config.h
@@ -63,6 +63,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function      format          argument */
-	{ datetime,      "%s",           "%F %T" },
-	{ battery_perc,  "%s",           "BAT1"},
+	{ keymap,        "|%s|",           NULL    },
+	{ datetime,      "%s|",           "%F %T" },
+	{ battery_perc,  "%s",           "BAT1"  },
 };
